5f33a9e897c9c41af1c07fa4f1ff112e7dad794a
RavCarvalho <ravennacarvalho098@gmail.com>
2024-09-20 14:48:45 -0300

    patrocars iniciado, ulid ja está funcionando.
    TODO: fazer metodos de adição de novas montadoras, ler e escrever montadoras de arquivos.

--- PatroCars/src/main/java/org/example/Main.java
+++ PatroCars/src/main/java/org/example/Main.java
@@ -1,9 +1,23 @@
 package org.example;
 import com.github.f4b6a3.ulid.UlidCreator;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.example.objects.Carmaker;
 
 public class Main {
     public static void main(String[] args) {
-        String ulid = UlidCreator.getUlid().toString();
-        System.out.println(ulid);
+        ArrayList<Carmaker> carmakers = new ArrayList<Carmaker>();
+        carmakers.add(new Carmaker("Fiat", "Alemanha", 2024));
+        carmakers.add(new Carmaker("BYD", "Canadá", 2012));
+        for (Carmaker carmaker: carmakers){
+            System.out.println(carmaker.toString());
+        }
+    }
+
+    public static void showCarmakers(Carmaker[] carmakers){
+        for (Carmaker carmaker : carmakers){
+            System.out.println(carmaker.toString());
+        }
     }
 }
--- PatroCars/src/main/java/org/example/objects/Carmaker.java
+++ PatroCars/src/main/java/org/example/objects/Carmaker.java
@@ -4,9 +4,54 @@ import com.github.f4b6a3.ulid.UlidCreator;
 public class Carmaker {
     // (id: ULID, nome: string, pais: string, ano_fundacao: int)
     private String id;
-    private String nome;
-    private String pais;
-    
+    private String name;
+    private String country;
+    private int year;
 
-    public Carmaker ()
+    public Carmaker (String name, String country, int year){
+        this.id = UlidCreator.getUlid().toString();
+        this.name = name;
+        this.country = country;
+        this.year = year;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return name + ": \n" +
+        "ID: " + id + "\n" +
+        "País: " + country + "\n" +
+        "Ano de fundação: " + year + "\n";
+    }
 }
